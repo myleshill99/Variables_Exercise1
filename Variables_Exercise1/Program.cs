c662c9eb28629a446b7e8b047878c8ea508a2cce
myleshill99 <myleshill95@gmail.com>
2023-05-25 16:25:42 -0400

    init commit

---
+++ Variables_Exercise1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Declare and initialize variables 
+string name = "Myles";
+int age = 22;
+char middleInitial = 'D';
+bool isOver18 = true;
+double currentTemp = 77.9;
+decimal currentPrice = 29.99M;
+
+Console.WriteLine($"Myles,"); 
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"MiddleInitial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
